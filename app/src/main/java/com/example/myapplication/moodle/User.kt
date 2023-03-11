883719a1dce79e53c52fcdca9db2a77a79223f85
abdallahabusalama <abdallahabusalama888@gmail.com>
2023-03-11 22:33:59 +0200

    finish

--- app/src/main/java/com/example/myapplication/moodle/User.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication.moodle
-
-import com.google.firebase.firestore.DocumentId
-
-data class User(@DocumentId var id:String ,var name:String ,var number:String,var address :String )
